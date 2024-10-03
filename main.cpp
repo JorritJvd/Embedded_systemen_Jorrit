9c4276c7b26207f0ba51d7797fc7143a9d7c525d
JorritJvd <165378902+JorritJvd@users.noreply.github.com>
2024-10-03 14:24:52 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
 
   // Create the BLE Device
-  BLEDevice::init("ESP32");
+  BLEDevice::init("ESP32_Jorrit");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
